4b3add2577f8ee078c1a1c43dadacdd3d60b9822
larbiboukert <larbiboukert@gmail.com>
2021-10-17 16:06:22 +0100

    ::green_heart:: fix build errors

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
-import Box from "@mui/system/Box";
 import Stack from "@mui/material/Stack";
 import IconButton from "@mui/material/IconButton";
 import AppBar from "@mui/material/AppBar";
@@ -14,7 +13,7 @@ const HomePage = (props: Props) => {
   return (
     <AppBar color="transparent">
       <Toolbar sx={{ justifyContent: "space-between", paddingY: "8px" }}>
-        <img src="./ig-logo.png" />
+        <img src="./ig-logo.png" alt="ig-logo" />
         <Stack direction="row" sx={{ alignItems: "center" }}>
           <IconButton aria-label="new-post">
             <AddBoxOutlinedIcon fontSize="large" />
